3ba60180f17c08ce5e39e3d8772f09957ace1b8f
shigeyuu <shigeyuu05@gmail.com>
2024-04-07 11:38:57 +0900

    Firestoreで使用する型名を短くしよう

---
+++ lib/typedefs/firestore_typedef.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+typedef SDMap = Map<String, dynamic>;
+typedef QSnapshot = QuerySnapshot<SDMap>;
+typedef MapQuery = Query<SDMap>;
+typedef QDoc = QueryDocumentSnapshot<SDMap>;
+typedef Doc = DocumentSnapshot<SDMap>;
+
+typedef FutureQSnapshot = Future<QSnapshot>;
+typedef FutureDoc = Future<Doc>;
+
+typedef DocRef = DocumentReference<SDMap>;
+typedef ColRef = CollectionReference<SDMap>;
