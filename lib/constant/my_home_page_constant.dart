8defda1a9cd67b2d5fd663c886d53ea1afd0e95e
shigeyuu <shigeyuu05@gmail.com>
2024-04-07 13:29:38 +0900

    Constantフォルダを作成しよう

---
+++ lib/constant/my_home_page_constant.dart
@@ -0,0 +1,5 @@
+class MyHomePageConstant {
+  static const createUserFailureMsg = "ユーザーの作成に失敗しました。";
+  static const readUserSuccessMsg = "ユーザーの読み取りが成功しました！";
+  static const readUserFailureMsg = "ユーザーの読み取りに失敗しました。";
+}
