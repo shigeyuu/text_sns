161e76c9ed7575fbc214da414466a77914e53328
shigeyuu <shigeyuu05@gmail.com>
2024-04-07 14:20:19 +0900

    Coreフォルダを作成しよう

---
+++ lib/core/firestore/query_core.dart
@@ -0,0 +1,8 @@
+import 'package:text_sns/core/firestore/col_ref_core.dart';
+import 'package:text_sns/typedefs/firestore_typedef.dart';
+
+class QueryCore {
+  static MapQuery publicUsersOrderByFollowerCount() =>
+      ColRefCore.publicUsersColRef()
+          .orderBy('follower_count', descending: true);
+}
