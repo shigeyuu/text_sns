ac1ae1decc73d8cd85e64bf4cfbf90ab91104729
shigeyuu <shigeyuu05@gmail.com>
2024-04-11 16:25:48 +0900

    環境変数の設定をしよう

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,8 +1,11 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'flavors.dart';
 
 import 'main.dart' as runner;
 
 Future<void> main() async {
   F.appFlavor = Flavor.dev;
+  await dotenv.load(fileName: "dev.env");
   await runner.main();
 }
