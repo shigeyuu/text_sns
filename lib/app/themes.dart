deed9f7f1513192a59dbd8b33b37145ac5df77c2
shigeyuu <shigeyuu05@gmail.com>
2024-04-13 10:07:55 +0900

    色を整えよう

--- lib/app/themes.dart
+++ lib/app/themes.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:text_sns/constant/colors.dart';
 
 class Themes {
   static ThemeData lightTheme() => ThemeData.light().copyWith(
-          appBarTheme: const AppBarTheme(
-        backgroundColor: Colors.white,
+      primaryColor: kPrimaryColorLightTheme,
+      scaffoldBackgroundColor: kContentColorLightTheme,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: kPrimaryColorLightTheme,
         elevation: 0.0,
         iconTheme: IconThemeData(color: Colors.black),
       ));
 
   static ThemeData darkTheme() => ThemeData.dark().copyWith(
-          appBarTheme: const AppBarTheme(
-        backgroundColor: Colors.black,
+      primaryColor: kPrimaryColorDarkTheme,
+      scaffoldBackgroundColor: kContentColorDarkTheme.withOpacity(0.8),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: kPrimaryColorDarkTheme,
         elevation: 0.0,
         iconTheme: IconThemeData(color: Colors.white),
       ));
