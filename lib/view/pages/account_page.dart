90c534bb9b3ffdaa10621576ccdede429e4d2785
shigeyuu <shigeyuu05@gmail.com>
2024-04-14 06:15:08 +0900

    ログアウト後のページを作成しよう

--- lib/view/pages/account_page.dart
+++ lib/view/pages/account_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:text_sns/controllers/auth_controller.dart';
 import 'package:text_sns/view/common/basic_page.dart';
 
 class AccountPage extends StatelessWidget {
@@ -7,10 +8,18 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const BasicPage(
+    final controller = AuthController.to;
+    return BasicPage(
       appBarTitle: 'アカウントページ',
       child: Center(
-        child: Align(alignment: Alignment.center, child: Text('アカウントページ')),
+        child: ListView(
+          children: [
+            ListTile(
+              title: const Text('ログアウト'),
+              onTap: controller.onSignOutButtonPressed,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/view/pages/logouted_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:text_sns/view/common/basic_page.dart';
+
+class LogoutedPage extends StatelessWidget {
+  const LogoutedPage({super.key});
+  static const path = "/logouted";
+  @override
+  Widget build(BuildContext context) {
+    return const BasicPage(
+      child: Align(
+        alignment: Alignment.center,
+        child: Text("ログアウトが完了しました。お疲れ様でした。",
+            style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
+      ),
+    );
+  }
+}
